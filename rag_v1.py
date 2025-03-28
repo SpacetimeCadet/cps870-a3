4f3ae66498276555cf2cbb46a3683db35e8a8594
Benedict Goes <benedictgoes@gmail.com>
2025-03-28 05:05:22 -0400

    un undid maybe

--- rag_v1.py
+++ rag_v1.py
@@ -8,6 +8,10 @@ import faiss
 import time
 from langchain_community.docstore.in_memory import InMemoryDocstore
 from langchain_community.vectorstores import FAISS
+from flask import Flask, render_template, request
+
+# Initialize User Interface
+app = Flask (__name__)
 
 load_dotenv()
 api_key = os.getenv("MISTRAL_API_KEY")
@@ -138,13 +142,14 @@ embeddings = np.array(get_embeddings_by_chunks(texts, 100))
 art_embeddings = np.array(get_embeddings_by_chunks(article_text_chunks, 20))
 
 
-while True:
-    user_input = input("You: ")
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-    if user_input.lower() == 'quit':
-        break
-
-    query = user_input
+@app.route("/get", methods=["GET", "POST"])
+def chatbot_response():
+    message = request.form["message"]
+    query = message
     prompt = "Using primarily the following information, please tell me "
     
     if classify_query(query) == 'articles':
@@ -164,4 +169,9 @@ while True:
         ]
     )
 
-    print(chat_response.choices[0].message.content)
+    #print(chat_response.choices[0].message.content)
+    return str(chat_response.choices[0].message.content)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+    
